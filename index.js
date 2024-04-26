64c09ad72e7080e4f25b1fafb73195104d996989
Karina Boronina <Gubashina@yandex.ru>
2024-04-26 16:39:09 +0300

    добавлена срезка, поливка и таймер роста цветов

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-const seedsList = document.body.querySelector('.seeds__list');
+const inventory = document.body.querySelector('.inventory');
 const garden_bed = document.body.querySelector('.garden_bed');
+let timers = [];
+
+// Открываем инвентарь
+const btnTools = document.body.querySelector('.btn__tools');
+btnTools.addEventListener('click', (e) => {
+    e.target.nextElementSibling.classList.toggle('active');
+});
 
 // Вешаем слушатель для отслеживания события перемещения семечки
-seedsList.addEventListener('drag', drag);
+inventory.addEventListener('dragstart', drag);
 
 // Проверяем семечку, что это картинка и берем ее имя из data-name
 function drag(e) {
-    e.preventDefault();
     let elem = e.target;
     if (elem.tagName === 'IMG') {
         let name = elem.dataset.name;
-        e.dataTransfer.setData('text/plain', name);
+        if (elem.closest('.seeds__item')) {
+            let time = elem.dataset.time;
+            e.dataTransfer.setData('name', name);
+            e.dataTransfer.setData('time', time);
+        }
+        if (elem.closest('.tools__item')) {
+            e.dataTransfer.setData('name', name);
+        }
     }
 }
 
@@ -18,26 +31,66 @@ garden_bed.addEventListener('dragover', (e) => e.preventDefault());
 // Слушаем событие посдки семечки в грядку
 garden_bed.addEventListener('drop', drop);
 
+// Проверяем событие что перетаскиваем и куда
 function drop(e) {
-    e.preventDefault();
+    let name = e.dataTransfer.getData('name');
+    // Что происходит если лунка пустая
     if (e.target.classList.contains('hole')) {
-        let img = document.createElement('img');
-        let flower_name = e.dataTransfer.getData('text/plain');
-        img.src = './images/sprout.svg';
-        e.target.appendChild(img);
-        flower_growth(img, flower_name);
+        if (name !== 'wateringСan' && name !== 'pruningShears') {
+            let img = document.createElement('img');
+            let time = Number(e.dataTransfer.getData('time'));
+            let timer = e.target.parentElement;
+            img.src = './images/sprout.svg';
+            img.alt = 'sprout';
+            e.target.appendChild(img);
+            start_timer(time, timer, img, name);
+        }
+        // Что происходит если поливаем росток в лунке
+    } else if (e.target.alt === 'sprout' && name === 'wateringСan') {
+        let { timerID, flower_name } = timers.find((el) => el.img === e.target);
+        let timer = e.target.closest('.timer');
+        timer.style = `background: conic-gradient( #684a13cb 360deg, yellowgreen 0deg)`;
+        e.target.src = `./images/${flower_name}.svg`;
+        e.target.alt = flower_name;
+        clearInterval(timerID);
+        timers = timers.filter((el) => el.timerID !== timerID);
+        // Что происходит если срезаем цветок в лунке
+    } else if (e.target.alt !== 'sprout' && name === 'pruningShears') {
+        e.target.remove();
     }
 }
 
-function flower_growth(img, flower_name) {
-    setTimeout(() => {
-        img.src = flower_name;
-    }, 5000);
+// Сам таймер роста цветка
+function start_timer(time, timer, img, flower_name) {
+    let deg_per_second = time_to_deg(time);
+    let deg = 0;
+
+    let timerID = setInterval(() => {
+        if (deg + deg_per_second < 360) {
+            deg += deg_per_second;
+            timer.style = `background: conic-gradient( #684a13cb ${deg}deg, yellowgreen 0deg)`;
+        } else {
+            timer.style = `background: conic-gradient( #684a13cb 360deg, yellowgreen 0deg)`;
+            img.src = `./images/${flower_name}.svg`;
+            img.alt = flower_name;
+            clearInterval(timerID);
+            timers = timers.filter((el) => el.timerID !== timerID);
+        }
+    }, 100);
+    timers.push({ timerID, img, flower_name });
+}
+
+function time_to_deg(time) {
+    let time_percent = 100 / time;
+    let deg_per_second = (360 * time_percent) / 100;
+    return deg_per_second;
 }
 
 // Срезаем цветочки
 const pruningShears = document.body.querySelector('.pruning_shears');
 pruningShears.addEventListener('click', () => {
     let flowers = document.querySelectorAll('.hole');
-    flowers.forEach((el) => (el.innerHTML = ''));
+    flowers.forEach((el) => {
+        if (el.firstElementChild?.alt !== 'sprout') el.innerHTML = '';
+    });
 });
